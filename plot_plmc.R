0618a4b70ef3ea8d7b6a7fba349be67f4b202b20
Aleix Lafita <aleixlafita@gmail.com>
2018-11-27 09:39:31 +0000

    Add plots for correlation matrices

---
+++ plot_plmc.R
@@ -0,0 +1,26 @@
+# Plot a PLMC (EV couplings) residue couplings matrix
+# Aleix Lafita - November 2018 - EIPP Bioinformatics course
+
+library(ggplot2)
+library(dplyr)
+
+# Parse the couplings data
+data = read.csv("mypfam/PF02470_full_couplings.txt", header = F, sep = " ")
+names(data) = c("RES_I", "FOCUS_AI", "RES_J", "FOCUS_AJ", "0", "SCORE")
+
+# Plot the correlation matrix 
+p = ggplot(data) +
+  geom_tile(aes(x = RES_I, y = RES_J, fill = SCORE)) +
+  geom_tile(aes(x = RES_J, y = RES_I, fill = SCORE)) +
+  theme_bw() +
+  theme(
+    legend.position = "right",
+    panel.grid = element_blank()
+  ) +
+  scale_fill_gradient2() +
+  scale_x_reverse()
+
+# Save the plot to a file
+pdf("mypfam/family_couplings.pdf", width = 5.5, height = 5)
+print(p)
+dev.off()
---
+++ plot_psicov.R
@@ -0,0 +1,30 @@
+# Plot a PSICOV residue correlation matrix
+# Aleix Lafita - November 2018 - EIPP Bioinformatics course
+
+library(ggplot2)
+library(dplyr)
+
+# Parse the couplings data
+data = read.csv("mypfam/PF02470_uniprot_hmm.psicov", header = F, sep = " ")
+names(data) = c("R1", "R2", "A", "B", "prob")
+
+# Just take the top 100 couplings
+data.filter = data %>%
+  top_n(100, prob)
+
+# Plot the correlation matrix 
+p = ggplot(data.filter) +
+  geom_point(aes(x = R1, y = R2, alpha = prob)) +
+  geom_point(aes(x = R2, y = R1, alpha = prob)) +
+  theme_bw() +
+  theme(
+    legend.position = "right"
+  ) +
+  scale_alpha_continuous(limits=c(0,1)) +
+  scale_x_reverse()
+
+# Save the plot to a file
+pdf("mypfam/family_psicov.pdf", width = 5.5, height = 5)
+print(p)
+dev.off()
+
